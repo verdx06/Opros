8c17d8b1beca11a6b4cf1c889a5feee0017fc643
gh <jh>
2023-11-22 01:33:23 +0700

    Initial Commit

---
+++ Opros/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Opros
+//
+//  Created by Виталий Багаутдинов on 22.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Opros/OprosApp.swift
@@ -0,0 +1,17 @@
+//
+//  OprosApp.swift
+//  Opros
+//
+//  Created by Виталий Багаутдинов on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct OprosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
